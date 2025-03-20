fee25501c0e5c9f6b5fe0bd963150cdcd5bda0b6
xhaadh <155063057+xhaadh@users.noreply.github.com>
2025-03-20 16:30:50 +0530

    codewithxhaadh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,8 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+          {/* <img src={brainwave} width={190} height={40} alt="Brainwave" /> */}
+          <p className="font-bold text-2xl">codewithxhaadh</p>
         </a>
 
         <nav
